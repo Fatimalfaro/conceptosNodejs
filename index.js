2916908ecae481e3a5293f66e2d41b8941563e16
Fátima Alfaro <fatialfaro99@gmail.com>
2026-06-03 20:28:29 -0300

    agregue un puerto en mi backend

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+//const express = required ('express') forma antigua de importar express
+
+//hago una instancia de express
+const app = express()
+//creo un puerto
+const PORT = 3000
+
+console.log("El primer mensaje de este backend")
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
